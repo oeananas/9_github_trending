135a35de663cf74bec17583c7a8bba646731c851
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-04-21 14:53:05 +0300

    Update github_trending.py
    fixed function (get_date_in_past), fixed script output

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,10 @@
 import requests
-import json
 import datetime
 
 
-def get_last_week_date():
+def get_date_in_past(days):
     now_date = datetime.date.today()
-    delta = datetime.timedelta(days=7)
+    delta = datetime.timedelta(days=days)
     last_week_date = now_date - delta
     return last_week_date.isoformat()
 
@@ -16,7 +15,7 @@ def get_trending_repositories(last_week_date, top_size):
         'https://api.github.com/search/repositories',
         params=params
     )
-    repositories = json.loads(response.content)['items']
+    repositories = response.json()['items']
     top_repositories = repositories[:top_size]
     return top_repositories
 
@@ -25,20 +24,20 @@ def get_open_issues_amount(repo_full_name):
     response = requests.get(
         'https://api.github.com/repos/{}/issues'.format(repo_full_name)
     )
-    for response_info in json.loads(response.content):
-        return response_info['number']
+    return len(response.json())
 
 
 if __name__ == '__main__':
-    last_week_date = get_last_week_date()
+    days = 7
     top_size = 20
-    trending_repositories = get_trending_repositories(last_week_date, top_size)
+    date_in_past = get_date_in_past(days)
+    trending_repositories = get_trending_repositories(date_in_past, top_size)
     print('list of the most trending repositories for last week: \n')
     try:
         for repo_info in trending_repositories:
             full_name = repo_info['full_name']
             repo_url = repo_info['html_url']
             issues_count = get_open_issues_amount(full_name)
-            print('url: {}, open issues: {}'.format(repo_url, issues_count))
+            print('url: {}, issues: {}'.format(repo_url, issues_count))
     except TypeError:
         exit('query limit exceeded')
